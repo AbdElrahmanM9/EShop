978a082f6e331294349d8c089593b0548b015309
AbdElrahman <mekky99.aa@gmail.com>
2022-06-18 09:23:39 +0200

    Change

--- EShop-Gp/ViewModel/ItemsList.cs
+++ EShop-Gp/ViewModel/ItemsList.cs
@@ -9,6 +9,7 @@ namespace EShop_Gp.ViewModel
     public class ItemsList
     {
         public IEnumerable<Items> Items { get; set; }
+        public Items Item { get; set; }
         public IEnumerable<Product> products { get; set; }
         public IEnumerable<Cart> carts { get; set; }
         public Slid Slid { get; set; }
